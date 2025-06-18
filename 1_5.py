d4fccb15b5c8208e0956d51a226d92cd2b7f7ffd
fuckupoff <anvesaru@gmail.com>
2025-06-18 17:22:21 +0700

    1_5

--- 1_5.py
+++ 1_5.py
@@ -1,4 +1,5 @@
-def sieve(lst):
-    return tuple(sorted(set(lst), reverse=True))
-
+def sieve(list):
+    nums = set(list)
+    nums = sorted(nums, reverse = True)
+    return tuple(nums)
 print(sieve([3, 5, 3, 8, 1, 7, 7, 1, 4, 9, 2, 2]))
