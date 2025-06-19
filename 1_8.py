203195860c10fdf4000b201fde354c687336c271
fuckupoff <anvesaru@gmail.com>
2025-06-19 18:06:08 +0700

    Update 1_8.py

--- 1_8.py
+++ 1_8.py
@@ -2,5 +2,5 @@ def date_dict(date_str):
     year, month, day = date_str.split("-")
     return{"year": year,"month": month,"day": day}
 
-date = "2093-10-10"
+date = "2025-12-31"
 print(date_dict(date))
